6e1ff4bdac8d4b35d3d3bfa57d36434edb6017c4
Lekkyjay <lekkyjay@yahoo.com>
2023-01-01 18:08:23 +0100

    Routes added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-
-import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import 'swiper/swiper.min.css'
+import './App.scss'
+import Detail from './pages/detail/Detail'
+import Catalog from './pages/Catalog'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
-      App
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:category' element={<Catalog />} />
+        <Route path='/:category/search/:keyword' element={<Catalog />} />
+        <Route path='/:category/:id' element={<Detail />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
