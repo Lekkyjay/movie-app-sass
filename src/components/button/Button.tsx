f3956ed46ec25deb3418942dbcc8e8ab2e964153
Lekkyjay <lekkyjay@yahoo.com>
2023-01-01 20:19:49 +0100

    Created Button component.

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './button.scss'
+
+interface IProps {
+  className: string
+  onClick?:() => void
+  children: React.ReactNode
+}
+
+export default function Button(props: IProps) {
+  return (
+    <button className={`btn ${props.className}`} onClick={props.onClick}>
+      {props.children}
+    </button>
+  )
+}
---
+++ src/components/button/OutlineButton.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Button'
+
+interface IProps {
+  className: string
+  onClick?:() => void
+  children: React.ReactNode
+}
+
+export default function OutlineButton(props: IProps) {
+  return (
+    <Button className={`btn-outline ${props.className}`} onClick={props.onClick}>
+      {props.children}
+    </Button>
+  )
+}
